116cd2926c06d8b1d0186b2520bf88fd99b19fb1
Vani Sachdev <sachdev.vani@gmail.com>
2023-02-18 09:20:22 -0800

    hi kelly

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import *
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
