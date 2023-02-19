0adc2cc26adb50dd1b258b10f89dc66ff7c3869f
Vani Sachdev <sachdev.vani@gmail.com>
2023-02-18 22:21:20 -0800

    change to rw

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 def create_image(url):
     img_data = requests.get(url).content
-    with open('static/media/imgs/imgs.jpg', 'wb') as handler:
+    with open('static/media/imgs/imgs.jpg', 'rw') as handler:
         handler.write(img_data)
 
 
